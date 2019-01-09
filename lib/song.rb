98014724db15c1f97a5cf0a25dae0270d875ecc7
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-09 05:00:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,11 @@ def all
   @@all
 end
 
+def self.all
+  @@all
+end
+end
+
 def artist_name
 if self.artist
   self.artist.name
@@ -17,7 +22,18 @@ else
   nil
 end
 end
-def self.all
-  @@all
-end
-end
+
+def self.find(name)     #class method detects instances from @@all class variable
+   self.all.detect { |artist| artist.name == name }
+ end
+
+ def self.create(name)   #class method creates & stores instances vs initializing
+   artist = Artist.new(name)
+   artist.save
+   artist
+ end
+
+ def print_songs #instance method iterates through @songs of an instance of Artist
+   self.songs.each {|song| song.name}
+ end
+ end
